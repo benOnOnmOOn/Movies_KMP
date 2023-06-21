079f63e5913eca29073592ebd635800e426fc710
Benedykt Ziobro <benedykt@backbase.com>
2023-06-21 16:08:06 +0200

    fix : try diffrent approach

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,7 +24,7 @@ gradleEnterprise {
 
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
         google {
             content {
