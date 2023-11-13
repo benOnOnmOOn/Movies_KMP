acd13e6add5563d68f323936ec32a4894a587332
Benedykt Ziobro <benedykt@backbase.com>
2023-11-13 09:18:49 +0100

    fix : update deps

--- data/database/src/commonMain/kotlin/com/bz/movies/kmp/database/di/DatabaseModule.kt
+++ data/database/src/commonMain/kotlin/com/bz/movies/kmp/database/di/DatabaseModule.kt
@@ -1,9 +1,5 @@
 package com.bz.movies.kmp.database.di
 
-import com.bz.movies.kmp.database.MoviesDB2
-import com.bz.movies.kmp.database.MoviesDB2Queries
 import org.koin.core.module.Module
-import org.koin.dsl.module
 
 expect val databaseModule: Module
-
