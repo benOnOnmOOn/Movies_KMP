906e77b6b9bf17b1b82412df4707cd7358ea05d6
Benedykt Ziobro <benedykt@backbase.com>
2024-02-26 16:22:34 +0100

    fix : update deps

--- android/presentation/screens/src/main/kotlin/com/bz/movies/presentation/components/MovieList.kt
+++ android/presentation/screens/src/main/kotlin/com/bz/movies/presentation/components/MovieList.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
