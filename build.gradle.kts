67551c0ef2e8ec2a54c746e7d330b730ce99616e
Benedykt Ziobro <benedykt.ziobro@gmail.com>
2024-06-21 12:47:26 +0200

    Exclude androidx fragment (#26)
    * progourd build optimalization

--- build.gradle.kts
+++ build.gradle.kts
@@ -215,6 +215,7 @@ fun <
             "META-INF/**",
             "**.properties",
             "kotlin-tooling-metadata.json",
+            "DebugProbesKt.bin",
         )
 }
 
@@ -241,6 +242,25 @@ fun BaseAppModuleExtension.baseConfig(project: Project) {
 
 subprojects {
     project.plugins.applyBaseConfig(project)
+    configurations.all {
+        exclude("org.jetbrains.kotlin", "kotlin-stdlib-jdk7")
+        exclude("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
+        exclude("com.google.code.findbugs", "jsr305")
+        exclude("com.google.errorprone", "error_prone_annotations")
+        exclude("androidx.legacy", "legacy-support-core-utils")
+        exclude("androidx.loader", "loader")
+        exclude("androidx.privacysandbox.ads", "ads-adservices-java")
+        exclude("androidx.privacysandbox.ads", "ads-adservices")
+        exclude("androidx.cursoradapter", "cursoradapter")
+        exclude("androidx.customview", "customview")
+        exclude("androidx.versionedparcelable", "versionedparcelable")
+        exclude("androidx.vectordrawable", "vectordrawable-animated")
+        exclude("androidx.vectordrawable", "vectordrawable")
+        exclude("androidx.drawerlayout", "drawerlayout")
+        exclude("androidx.fragment", "fragment")
+        exclude("androidx.fragment", "fragment-ktx")
+        exclude("org.checkerframework", "checker-qual")
+    }
 }
 // endregion
 
