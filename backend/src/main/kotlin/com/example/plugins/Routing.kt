2a091bcd23ecd289b7f034d75d67cafca0b04048
Benedykt Ziobro <benedykt.ziobro@gmail.com>
2024-07-10 14:48:30 +0200

    create sample backend

--- backend/src/main/kotlin/com/example/plugins/Routing.kt
+++ backend/src/main/kotlin/com/example/plugins/Routing.kt
@@ -7,7 +7,6 @@ import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 
-
 fun Application.configureRouting() {
     routing {
         get("/") {
