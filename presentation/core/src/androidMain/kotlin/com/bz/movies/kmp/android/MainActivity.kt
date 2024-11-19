7787bee4f8d26a9e874d9e40a1dcc900abc43fbd
Benedykt Ziobro <benedykt.ziobro@gmail.com>
2024-11-19 11:48:32 +0100

    Add proper error handling for data store

--- presentation/core/src/androidMain/kotlin/com/bz/movies/kmp/android/MainActivity.kt
+++ presentation/core/src/androidMain/kotlin/com/bz/movies/kmp/android/MainActivity.kt
@@ -6,11 +6,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.bz.movies.kmp.main.MainScreen
-import org.koin.compose.KoinContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { KoinContext { MainScreen() } }
+        setContent { MainScreen() }
     }
 }
