fd666554a882fd56dfd8347b3973d09e32bc8a0d
Benedykt Ziobro <benedykt.ziobro@gmail.com>
2024-11-22 16:02:40 +0100

    Replace SQLDelitght with room
    Signed-off-by: Benedykt Ziobro <benedykt.ziobro@gmail.com>

--- data/room/build.gradle.kts
+++ data/room/build.gradle.kts
@@ -72,7 +72,10 @@ kotlin {
 
 dependencies {
     dependencies {
-        add("kspCommonMainMetadata", libs.androidx.room.compiler)
+        add("kspAndroid", libs.androidx.room.compiler)
+        add("kspIosSimulatorArm64", libs.androidx.room.compiler)
+        add("kspIosX64", libs.androidx.room.compiler)
+        add("kspIosArm64", libs.androidx.room.compiler)
     }
 }
 
