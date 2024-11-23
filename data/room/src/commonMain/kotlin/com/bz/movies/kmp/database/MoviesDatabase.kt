6ab5f80fcefb41c34a0661f631854f2dec1c41f8
Benedykt Ziobro <benedykt.ziobro@gmail.com>
2024-11-23 17:03:26 +0100

    Replace SQLDelitght with room
    Signed-off-by: Benedykt Ziobro <benedykt.ziobro@gmail.com>

--- data/room/src/commonMain/kotlin/com/bz/movies/kmp/database/MoviesDatabase.kt
+++ data/room/src/commonMain/kotlin/com/bz/movies/kmp/database/MoviesDatabase.kt
@@ -1,8 +1,10 @@
 package com.bz.movies.kmp.database
 
 import androidx.room.AutoMigration
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
 import com.bz.movies.kmp.database.dao.MovieDAO
 import com.bz.movies.kmp.database.dao.PlayingNowMovieDAO
 import com.bz.movies.kmp.database.dao.PopularMovieDAO
@@ -12,6 +14,9 @@ import com.bz.movies.kmp.database.entity.PopularMovieEntity
 
 internal const val DATABASE_NAME = "MoviesDB"
 
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+internal expect object MyDatabaseCtor : RoomDatabaseConstructor<MoviesDatabase>
+
 @Database(
     entities = [MovieEntity::class, PlayingNowMovieEntity::class, PopularMovieEntity::class],
     version = 2,
@@ -20,6 +25,7 @@ internal const val DATABASE_NAME = "MoviesDB"
         AutoMigration(from = 1, to = 2),
     ],
 )
+@ConstructedBy(MyDatabaseCtor::class)
 internal abstract class MoviesDatabase : RoomDatabase() {
     abstract fun movieDAO(): MovieDAO
 
