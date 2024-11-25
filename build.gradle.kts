38bfe182ed38e613a2d4e5e68fba36a8bdcccad0
Benedykt Ziobro <benedykt.ziobro@gmail.com>
2024-11-25 14:39:50 +0100

    Try to run lints and update baseline when creating PR
    Signed-off-by: Benedykt Ziobro <benedykt.ziobro@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -175,7 +175,7 @@ fun <
 
     lint {
         baseline = project.file("lint-baseline.xml")
-        disable += listOf("NewerVersionAvailable", "GradleDependency")
+        disable += listOf("NewerVersionAvailable", "GradleDependency", "ObsoleteLintCustomCheck")
         abortOnError = true
         checkAllWarnings = true
         warningsAsErrors = true
