33d76143fea2f6d116a5b97771640348c2f848be
Benedykt Ziobro <benedykt.ziobro@gmail.com>
2024-12-05 10:48:19 +0100

    Try to checkout to latest head Signed-off-by: Benedykt Ziobro <benedykt.ziobro@gmail.com>
    Signed-off-by: Benedykt Ziobro <benedykt.ziobro@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -175,7 +175,7 @@ fun <
 
     lint {
         baseline = project.file("lint-baseline.xml")
-        disable += listOf("NewerVersionAvailable", "GradleDependency", "ObsoleteLintCustomCheck")
+        disable += listOf("ObsoleteLintCustomCheck")
         abortOnError = true
         checkAllWarnings = true
         warningsAsErrors = true
