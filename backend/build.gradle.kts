e0dc7abeab75c50174e2ba3dbc519f7eabd7a742
Benedykt Ziobro <benedykt.ziobro@gmail.com>
2024-12-09 15:28:35 +0100

    Tru to use cio for backend Signed-off-by: Benedykt Ziobro <benedykt.ziobro@gmail.com>
    Signed-off-by: Benedykt Ziobro <benedykt.ziobro@gmail.com>

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,38 +1,53 @@
 plugins {
     alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.dependency.guard)
-    alias(libs.plugins.jetbrains.kotlin.jvm)
     alias(libs.plugins.com.autonomousapps.dependency.analysis) apply true
     alias(libs.plugins.kotlin.serialization)
 }
 
-group = "com.bz.movies"
-version = "0.0.1"
+kotlin {
+    val hostOs = System.getProperty("os.name")
+    val arch = System.getProperty("os.arch")
+    val nativeTarget =
+        when {
+            hostOs == "Mac OS X" && arch == "x86_64" -> macosX64("native")
+            hostOs == "Mac OS X" && arch == "aarch64" -> macosArm64("native")
+            hostOs == "Linux" -> linuxX64("native")
+            // Other supported targets are listed here: https://ktor.io/docs/native-server.html#targets
+            else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+        }
 
-application {
-    mainClass.set("com.example.ApplicationKt")
+    nativeTarget.apply {
+        binaries {
+            executable {
+                entryPoint = "com.example.main"
+            }
+        }
+    }
 
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
-}
-
-dependencies {
-    implementation(libs.kotlin.stdlib)
-    implementation(libs.ktor.server.core.jvm)
-    implementation(libs.ktor.server.netty.jvm)
-    implementation(libs.io.ktor.http)
-    implementation(libs.ktor.server.content.negotiation)
-    implementation(libs.io.ktor.serialization.kotlinx.json)
-    implementation(libs.io.ktor.serialization)
-    implementation(libs.io.ktor.utils)
-    implementation(libs.kotlinx.serialization.core)
-    implementation(libs.kotlinx.serialization.json)
-
-    runtimeOnly(libs.logback.classic)
-    implementation(libs.ktor.server.config.yaml)
+    sourceSets {
+        val nativeMain by getting {
+            dependencies {
+                implementation(libs.kotlin.stdlib)
+                implementation(libs.ktor.server.cio)
+//                implementation("io.ktor:ktor-server-cio:$ktor_version")
+                implementation(libs.io.ktor.http)
+                implementation(libs.ktor.server.content.negotiation)
+                implementation(libs.io.ktor.serialization.kotlinx.json)
+                implementation(libs.io.ktor.serialization)
+                implementation(libs.io.ktor.utils)
+                implementation(libs.kotlinx.serialization.core)
+                implementation(libs.kotlinx.serialization.json)
+            }
+        }
+    }
 }
 
 dependencyGuard {
+
     // All dependencies included in Production Release APK
-    configuration("runtimeClasspath")
+    configuration("nativeCInterop") {
+        tree = true
+    }
 }
