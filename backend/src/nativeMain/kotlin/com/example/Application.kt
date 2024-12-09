cb9be1b93765f947405f73c02dbd1258083be08c
Benedykt Ziobro <benedykt.ziobro@gmail.com>
2024-12-09 16:05:58 +0100

    Try to use cio for backend  (#58)
    * Tru to use cio for backend
    
    Signed-off-by: Benedykt Ziobro <benedykt.ziobro@gmail.com>
    Co-authored-by: benOnOnmOOn <benOnOnmOOn@users.noreply.github.com>

--- backend/src/nativeMain/kotlin/com/example/Application.kt
+++ backend/src/nativeMain/kotlin/com/example/Application.kt
@@ -3,15 +3,14 @@ package com.example
 import com.example.plugins.configureRouting
 import io.ktor.http.ContentType
 import io.ktor.serialization.kotlinx.json.json
-import io.ktor.server.application.Application
 import io.ktor.server.application.install
+import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import kotlinx.serialization.json.Json
 
-fun main(args: Array<String>) {
-    embeddedServer(Netty, port = 9080) {
+fun main() {
+    embeddedServer(CIO, port = 9080) {
         install(ContentNegotiation) {
             json(
                 Json {
@@ -23,8 +22,4 @@ fun main(args: Array<String>) {
         }
         configureRouting()
     }.start(wait = true)
-}
-
-fun Application.module() {
-    configureRouting()
 }
