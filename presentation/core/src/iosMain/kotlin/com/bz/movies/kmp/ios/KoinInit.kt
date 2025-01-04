b1d91c810da057c21e816cbb7ab2ad6d6d829c56
Benedykt Ziobro <benedykt.ziobro@gmail.com>
2025-01-04 12:34:11 +0100

    Use koin lazy modules
    Signed-off-by: Benedykt Ziobro <benedykt.ziobro@gmail.com>

--- presentation/core/src/iosMain/kotlin/com/bz/movies/kmp/ios/KoinInit.kt
+++ presentation/core/src/iosMain/kotlin/com/bz/movies/kmp/ios/KoinInit.kt
@@ -1,9 +1,10 @@
 import com.bz.movies.kmp.di.presentationModule
 import org.koin.core.context.startKoin
+import org.koin.core.lazyModules
 
 @Suppress("FunctionName")
 fun InitKoin() {
     startKoin {
-        modules(presentationModule)
+        lazyModules(presentationModule)
     }
 }
