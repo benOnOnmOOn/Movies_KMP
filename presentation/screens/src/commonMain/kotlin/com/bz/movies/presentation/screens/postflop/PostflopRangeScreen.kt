4ff6f825baa3c6a4df9f531efdc1caf3d431845f
Benedykt Ziobro <benedykt.ziobro@gmail.com>
2025-03-18 12:12:46 +0100

    Slider
    Signed-off-by: Benedykt Ziobro <benedykt.ziobro@gmail.com>

--- presentation/screens/src/commonMain/kotlin/com/bz/movies/presentation/screens/postflop/PostflopRangeScreen.kt
+++ presentation/screens/src/commonMain/kotlin/com/bz/movies/presentation/screens/postflop/PostflopRangeScreen.kt
@@ -13,10 +13,14 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -112,6 +116,22 @@ internal fun PostflopRangeScreen(
                 onClick = { viewmodel.sendEvent(RangeEditEvent.Clear) }) {
                 Text(text = stringResource(Res.string.postflop_range_clear))
             }
+        }
+
+        Row(
+            modifier = modifier.padding(8.dp),
+        ) {
+            var sliderPosition by remember { mutableFloatStateOf(0f) }
+
+            Slider(
+                modifier = modifier.padding(8.dp).weight(1f),
+                value = sliderPosition,
+                onValueChange = { sliderPosition = it }
+            )
+            Text(
+                modifier = modifier.fillMaxWidth(0.1f),
+                text = sliderPosition.roundToDecimals(2).toString()
+            )
         }
     }
 }
