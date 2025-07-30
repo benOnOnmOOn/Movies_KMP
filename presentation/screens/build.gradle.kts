b59fcfe0fb04371388561302c24881cfe24a30e4
Benedykt Ziobro <benedykt.ziobro@gmail.com>
2025-07-30 08:38:06 +0200

    Update rules

--- presentation/screens/build.gradle.kts
+++ presentation/screens/build.gradle.kts
@@ -79,10 +79,6 @@ kotlin {
             api(libs.androidx.navigation.runtime)
             api(libs.kotlin.stdlib)
 
-            implementation(libs.androidx.compose.ui.tooling)
-            implementation(libs.androidx.compose.ui.tooling.preview)
-            implementation(libs.kotlinx.compose.ui.tooling.preview)
-
             implementation(libs.androidx.lifecycle.common)
             implementation(libs.androidx.lifecycle.runtime.compose)
             implementation(libs.androidx.navigation.compose)
